1b1fd958d3a7e1f47bf1a6ed13afa6d026550dd6
LostArk2018 <test99555672@gmail.com>
2018-11-09 10:40:54 +0200

    Add author info & q date

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -5,83 +5,82 @@ namespace App\Http\Controllers;
 use App\Question;
 use Illuminate\Http\Request;
 
-class QuestionsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $questions = Question::latest()->paginate(5);
-        
-        return view('questions.index', compact('questions'));
-    }
+class QuestionsController extends Controller {
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index() {
+    //\DB::enableQueryLog();
+    
+    $questions = Question::with('user')->latest()->paginate(5);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return view('questions.index', compact('questions'));
+    
+    //view('questions.index', compact('questions'))->render();
+    //dd(\DB::getQueryLog());
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Question $question)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create() {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request) {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Question $question)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Question $question) {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Question $question)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Question $question) {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Question $question) {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Question $question) {
+    //
+  }
+
 }
--- app/Question.php
+++ app/Question.php
@@ -4,21 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
-{
-    protected $fillable = ['title', 'body'];
+class Question extends Model {
 
-    public function user()
-    {
-        //$question = Question::find(1);
-        //return $question->user->email;
+  protected $fillable = ['title', 'body'];
 
-        return $this->belongsTo(User::class);
-    }
+  public function getCreatedDateAttribute() {
+    return $this->created_at->diffForHumans();
+  }
+  
+  public function getUrlAttribute() {
+    return route("questions.show", $this->id);
+  }
 
-    public function setTitleAttribute(string $value) : void
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
+  public function setTitleAttribute(string $value): void {
+    $this->attributes['title'] = $value;
+    $this->attributes['slug'] = str_slug($value);
+  }
+
+  public function user() {
+    //$question = Question::find(1);
+    //return $question->user->email;
+
+    return $this->belongsTo(User::class);
+  }
+
 }
